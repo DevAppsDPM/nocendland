7e8307a777a26e127173f49839c729c1dc702a17
DevAppsDPM <devappsdpm@gmail.com>
2025-01-23 16:00:37 +0100

    test nixpacks.toml

--- nocendland/src/environments/environment.ts
+++ nocendland/src/environments/environment.ts
@@ -1,5 +1,5 @@
 ﻿export const environment = {
-  production: true,
+  production: false,
   supabaseUrl: 'https://hzwxzvupsctlyrqcdofi.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6d3h6dnVwc2N0bHlycWNkb2ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc4MTI0OTgsImV4cCI6MjA0MzM4ODQ5OH0.2G7yLGeJcbcSB8CPmVP1_7MZxr0C2qhOaQvJ4nvp-zc',
 }
